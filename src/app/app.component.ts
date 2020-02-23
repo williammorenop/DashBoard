92de4fc4be3d28970eb97fd64c53c4eb4be4b17b
William Moreno <williammoreno@javeriana.edu.co>
2020-02-23 17:13:01 -0500

    add 1 chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as c3 from 'c3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'DashboardC3';
+  data2 = [
+    ['Enero', 10, 20, 10, 20, 15, 25],
+    ['Febrero', 1000, 20, 10, 40, 15, 25]
+  ];
+  tipo = '';
+
+  hola() {
+    console.log(this.tipo);
+    console.log(this.data2[0][2]);
+
+    const data = {
+      columns: [],
+      types: null
+    };
+    data.columns = this.data2;
+    data.types = {
+      Enero: '', // ADD
+      Febrero: ''
+    };
+    data.types.Febrero = this.tipo;
+    data.types.Enero = this.tipo;
+
+    const chart = c3.generate({
+      bindto: '#chart1',
+      data
+    });
+
+    chart.flush();
+  }
+
+  ngOnInit() {
+    const data = {
+      columns: [],
+      types: null
+    };
+    data.columns = this.data2;
+    data.types = {
+      Enero: 'bar', // ADD
+      Febrero: 'bar'
+    };
+    data.types.Febrero = this.tipo;
+
+    const chart = c3.generate({
+      bindto: '#chart1',
+      data
+    });
+  }
 }
