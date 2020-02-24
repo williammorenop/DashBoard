cd533f6edb762f9f740fb4b8420bfe65d9003baa
William Moreno <williammoreno@javeriana.edu.co>
2020-02-23 20:43:13 -0500

    Add area chart

--- src/app/components/area/area.component.ts
+++ src/app/components/area/area.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as c3 from 'c3';
 
 @Component({
   selector: 'app-area',
@@ -10,6 +11,21 @@ export class AreaComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+    const chart = c3.generate({
+      bindto: '#chart3',
+      data: {
+        columns: [
+          ['data1', 300, 350, 300, 0, 0, 0],
+          ['data2', 130, 100, 140, 200, 150, 50]
+        ],
+        types: {
+          data1: 'area',
+          data2: 'area-spline'
+        }
+      }
+    });
+
   }
 
 }
